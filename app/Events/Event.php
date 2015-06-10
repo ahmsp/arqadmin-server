873e6cadcf32e76cfd7d6e864725783ecf4ed229
Alexandre Cyro <cyro.alex@gmail.com>
2015-06-10 20:54:13 -0300

    Initial commmit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ArqAdmin\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ArqAdmin\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
